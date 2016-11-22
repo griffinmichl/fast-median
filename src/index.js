d5c216b84a6c02ba31f70c4de30147cdada98172
Alexey Gusev <mad.gooze@gmail.com>
2016-11-23 02:22:23 +0300

    add benckmark && noForceCheck param

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import kthSmallest from 'kth-smallest'
 import isNumber from 'is-number'
 
-function median(nums) {
-  if (!Array.isArray(nums)) {
-    throw new TypeError(`nums should be array, got ${typeof nums}`)
+function median(nums, noForceCheck) {
+  if (!noForceCheck) {
+    if (!Array.isArray(nums)) {
+      throw new TypeError(`nums should be array, got ${typeof nums}`)
+    }
+
+    if (!nums.every(isNumber)) {
+      throw new TypeError('nums array must contain only numbers')
+    }
+
+    if (nums.length === 0) {
+      return undefined
+    }
   }
 
-  if (!nums.every(isNumber)) {
-    throw new TypeError('nums array must contain only numbers')
-  }
-
-  if (nums.length === 0) {
-    return undefined
-  }
 
   const middleIndex = Math.floor(nums.length / 2)
   const middleElementRight = kthSmallest(nums, middleIndex)
